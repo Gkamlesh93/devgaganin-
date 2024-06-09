b542caefed2b9de67c0940c72915572b4b26ab69
Gkamlesh93 <vishalgupta00995@gmail.com>
2024-06-09 14:15:03 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -16,7 +16,7 @@ logging.getLogger("telethon").setLevel(logging.WARNING)
 API_ID = "10000844" #config("API_ID", default=None, cast=int)
 API_HASH = "776f257fc1d1f8aa4aea9dd35d10a45b" #config("API_HASH", default=None)
 BOT_TOKEN = "7311703567:AAHhEoFLB1k84UQxXib1HLnWa0IIHbFfmXA" #config("BOT_TOKEN", default=None)
-SESSION = "BQF2s3oAKwMrDulQAieZ0_lxoaCiH2fepp95u_lwzQVAz9hrmnWmULIrG4QAbzo2CCfmPsuB49gF_c8epQryM_oSzaHVPn_HXQFPHsCwCOqhU4ZZjZm5uOaXwKarzqNmly3wQUav1BPg0GidvJN1ztrmh1XMGSbNb8StG_v2DF49zjdibeOg2LCoU0ckjwEYpqdjxhlTPedOkCflCssMDJZBdoMXT9cFiiOYwGOSwXAsSu_QQQG8N05NIA153qwePXeTHYRyC6ac5N9f0LPNSYwgeaB6aKV5EgcmIyLj7a0hhDn4YBwxZp1XhhI1-XxY0Ex37eBnctI-rCQ_9ufiDdxQCmKwEAAAAAFsWSMcAA" #config("SESSION", default=None)
+SESSION = "AQCUKpYPlvNDeRJe_rzI76PW60zHvPX3fgGM8hs8_TjB_8Fm1DR9tQrWrOwb6OCQ93J7SPOPJm_8LRBJ9dBxDbMvqL_WmdmTk8lSYcnODREzWaQptYFcSaAtK5Ou0v_VtZuzF_0LQOoZkfYQRIGSEz40w5JuJ52htVC75sJA9Kn-unxB0mmT6gGxFg7uaT3UclbSgcG7D-LrlLIK0i1HWYS4depqM7HA9HYxD8Y_G1yaWBmh4ZMKPcnDc9BLDerK_h078kUsDjUUNR4e33eLtjngXkWHNblJElXXknT6xfzb4RgbpdE7KxtVl1i4QifE4Q-fs-4ApC6YQoabjvUDGH6NAAAAAamsXEEA" #config("SESSION", default=None)
 FORCESUB = "Restricted_content_bot1" #config("FORCESUB", default=None)
 AUTH = "7141612609" #config("AUTH", default=None)
 SUDO_USERS = []
